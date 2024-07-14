d5bc79d980237c048b425eb1dbef1cd573cd246d
zaphir21 <149177599+zaphir21@users.noreply.github.com>
2024-07-14 15:22:26 +0200

    Ajout systeme de check si c'est un bot ou un utilisateur + embed

--- events/StatusUpdate.js
+++ events/StatusUpdate.js
@@ -1,20 +1,29 @@
-const {  Events, GuildChannelManager } = require('discord.js');
+const {  Events, GuildChannelManager, Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const { execute } = require('./interactionCreate');
+
 module.exports = {
     name: Events.PresenceUpdate,
     async execute(oldPresence, newPresence){
         const user = newPresence.member.user;
+        if (user.bot) return;
         let activite = newPresence.activities.filter(act => act.type == 4)
         activite = activite[0]
         let role = "1249990069826097202"
         msg = "eisfr"
+        const embed = new EmbedBuilder()
+
+        const channel = newPresence.guild.channels.cache.get("1249995016873709589")
         try{
         if (!newPresence.member.roles.cache.has(role)) {
-            
+
                 if (activite.state.includes(msg)) {
                     // alors, ajouter le role
                     newPresence.member.roles.add(role, "Soutien")
-                    console.log(`${user.tag} a reçu le rôle soutien.`);
+                    embed.setTitle("Nouveau Soutien")
+                    embed.setDescription(`${user.username} vient de recevoir le rôle soutien !`)
+                    embed.setColor("#00FF00")
+                    embed.setFooter("Merci à ce G.O.A.T du nom Zaphir.21 pour ce merveilleux bot !")
+                    channel.send({ embeds: [embed] })
                 }
             
         }
@@ -24,7 +33,11 @@ module.exports = {
                     return // s'il contient un msg de soutien
                 }
                 newPresence.member.roles.remove(role, "Soutien")
-                console.log(`${user.tag} a perdu le rôle soutien.`);
+                embed.setTitle("Soutien Perdu")
+                    embed.setDescription(`${user.username} vient de perdre le rôle soutien ce noubs!`)
+                    embed.setColor("#FF0000")
+                    embed.setFooter("Merci à ce G.O.A.T du nom Zaphir.21 pour ce merveilleux bot !")
+                    channel.send({ embeds: [embed] })
             
 
         }
