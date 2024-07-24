64538fb81539148eacc07575521b086e35dc8970
zaphir21 <149177599+zaphir21@users.noreply.github.com>
2024-07-24 16:34:58 +0200

    Update status  for the use of the support role and the support message which is in the config.jso

--- events/StatusUpdate.js
+++ events/StatusUpdate.js
@@ -1,6 +1,6 @@
 const {  Events, GuildChannelManager, Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const { execute } = require('./interactionCreate');
-
+let { soutien } = require('../config.json')
 module.exports = {
     name: Events.PresenceUpdate,
     async execute(oldPresence, newPresence){
@@ -8,16 +8,17 @@ module.exports = {
         if (user.bot) return;
         let activite = newPresence.activities.filter(act => act.type == 4)
         activite = activite[0]
-        let role = "1249990069826097202"
-        msg = "eisfr"
+        msg = soutien.msg
+        soutienRole = soutien.role
         const embed = new EmbedBuilder()
-        embed.setFooter({text: "Merci à ce G.O.A.T du nom Zaphir.21 pour ce merveilleux bot !"})
+        embed.setFooter({text: "Merci à ce G.O.A.T du nom de Zaphir.21 pour ce merveilleux bot !"})
+        embed.setTimestamp()
         const channel = newPresence.guild.channels.cache.get("1249995016873709589")
         try{
-        if (!newPresence.member.roles.cache.has(role)) {
+        if (!newPresence.member.roles.cache.has(soutienRole)) {
                 if (activite.state.includes(msg)) {
                     // alors, ajouter le role
-                    newPresence.member.roles.add(role, "Soutien")
+                    newPresence.member.roles.add(soutienRole, "Soutien")
                     embed.setTitle("Nouveau Soutien")
                     embed.setDescription(`${user.username} vient de recevoir le rôle soutien !`)
                     embed.setColor("#00FF00")
@@ -30,7 +31,7 @@ module.exports = {
                 if (activite.state.includes(msg)) {
                     return // s'il contient un msg de soutien
                 }
-                newPresence.member.roles.remove(role, "Soutien")
+                newPresence.member.roles.remove(soutienRole, "Soutien")
                 embed.setTitle("Soutien Perdu")
                     embed.setDescription(`${user.username} vient de perdre le rôle soutien ce noubs!`)
                     embed.setColor("#FF0000")
@@ -39,12 +40,11 @@ module.exports = {
 
         }
         }catch (e){
-            if(!oldPresence.member.roles.cache.has(role)) return
-            newPresence.member.roles.remove(role, "Soutien")
+            if(!oldPresence.member.roles.cache.has(soutienRole)) return
+            newPresence.member.roles.remove(soutienRole, "Soutien")
             embed.setTitle("Soutien Perdu")
                 embed.setDescription(`${user.username} vient de perdre le rôle soutien ce noubs!`)
                 embed.setColor("#FF0000")
-                channel.send({ embeds: [embed] })
+                channel.send({ embeds: [embed] })}
         }
     }
-}
